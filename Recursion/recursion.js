b31e654b54a1aa77c58dd5683483a509519a9ac5
Amaar Sana <87737710+am44r@users.noreply.github.com>
2022-03-07 21:19:48 -0500

    Update recursion.js

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -1,19 +1,13 @@
-"use strict";
-
-function countdown(n) {
-  if (n === 0) return null;
-  // Creating the base case (if n is 0, return null)
-  else console.log(n); // If n is not 0, print n
-  countdown(n - 1); // Recursive call for n - 1 (if n is 5 then it will call countdown on n is 4)
+function doubleCountdownHelper(n) {
+  if (n <= 0) return 0;
+  console.log(n);
+  return doubleCountdownHelper(n - 1);
 }
 
 function doubleCountdown(n) {
-  // To print the second countdown I call the previous countdown within it
-  countdown(n); // Call the first countdown for n
-  countdown(n - 1); // Call it again for n - 1
-  console.log(0); // At the end print 0
-
-  return null;
+  doubleCountdownHelper(n);
+  doubleCountdownHelper(n - 1);
+  console.log(0);
 }
 
-doubleCountdown(5);
+doubleCountdown(6);
