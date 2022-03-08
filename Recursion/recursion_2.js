d0462646d3e027d996731a4a63c450c3a35d80c1
Amaar Sana <87737710+am44r@users.noreply.github.com>
2022-03-07 23:37:40 -0500

    Update recursion_2.js

--- Recursion/recursion_2.js
+++ Recursion/recursion_2.js
@@ -5,10 +5,10 @@ evenly divisible by n recursivley. Your extracted array should be
 sorted in ascending numerical order.
 */
 
-function extractEvenDivisible(numbers, n) {
+function extractEvenlyDivisible(numbers, n) {
   if (numbers.length === 0) return [];
-  if (numbers[0] % n !== 0) return extractEvenDivisible(numbers.slice(1), n);
-  return [numbers[0]].concat(extractEvenDivisible(numbers.slice(1), n));
+  if (numbers[0] % n !== 0) return extractEvenlyDivisible(numbers.slice(1), n);
+  return [numbers[0]].concat(extractEvenlyDivisible(numbers.slice(1), n));
 }
 
-console.log(extractEvenDivisible([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(extractEvenlyDivisible([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
